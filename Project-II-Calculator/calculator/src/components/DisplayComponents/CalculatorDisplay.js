542f6e4896675d57bda5e0bb381f852ae7c80169
Brandon L. Borrero <borrero509@outlook.com>
2019-02-12 13:19:31 -0800

    Display made

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = () => {
+    return (
+        <button className="CalculatorDisplay">0</button>
+    );
+  };
+
+export default CalculatorDisplay;
+
