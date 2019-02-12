9e429f0617fdf50a67e3828650b29b0f0e07bb75
Brandon L. Borrero <borrero509@outlook.com>
2019-02-12 14:07:31 -0800

    set up NumberButton

---
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/Buttons.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Button.css';
+
+  const Buttons = () => {
+    return (
+        <div className="Buttons">
+       
+        </div>
+    );
+  };
+
+  
+  export default Buttons;
+  
